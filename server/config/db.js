b73081143643917b92d9877c71a2ad4bb2973a47
Chris Saden <csaden@gmail.com>
2015-08-06 14:41:00 -0700

    beginning files for server

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+var Mongo    = require('mongo'),
+    mongoose = require('Mongoose');
+
+
+
+module.exports = db;
---
+++ server/config/server.js
@@ -0,0 +1,35 @@
+//========================
+// modules
+//========================
+
+var express        = require('express'),
+    bodyParser     = require('body-parser'),
+    methodOverride = require('method-override'),
+    db             = require('./config/db');
+
+//========================
+// configuration
+//========================
+
+
+
+// set the port
+var port = process.env.PORT || 8080;
+
+var app = express();
+
+// configure our server with all the middleware and and routing
+require('./config/middleware.js')(app, express);
+
+//========================
+// start app
+//========================
+
+// startup our app at http://localhost:8080
+app.listen(port);
+
+// shoutout to the user
+console.log('Server magic happens on port ' + port);
+
+// export our app for testing and flexibility, required by index.js
+module.exports = app;
