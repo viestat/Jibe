03bc0cca5151c223d29877ec91e751e34e004371
Max Ince <ince.max@gmail.com>
2015-08-06 18:23:27 -0700

    commit for the evening

--- js/controllers.js
+++ js/controllers.js
@@ -1,12 +1,24 @@
 angular.module('djBooth.controllers', [])
 // controller for handling the search function
 .controller('searchController', function ($scope, $window, searchSpotify){
-  $scope.results = [{'songname': 'semi-charmed life', 'votes':  3}];
 
-  $scope.getResults= function(){
+  // the results array that houses the songs currently in the queue
+  $scope.results = [{songname: 'semi-charmed life', votes:  3}];
+  // this function calls the getData function in the SearchSpotify factory and returns the selected song
+  // this song is then added to the queue
+  $scope.getSong = function(){
     SearchSpotify.getData($scope.song, function(data){
+      data.votes = 0;
       $scope.results.push(data);
     })
   }
 
+})
+.directive('updateSearch', function(){
+  return {
+    link : function(scope, elem, attrs){
+      console.log('hi');
+    }
+  }
+  
 })
