d8fe9c3a77d209805e1a39997192c2d116c8aa1b
Max Ince <ince.max@gmail.com>
2015-08-07 12:13:27 -0700

    added some more comments

--- js/controllers.js
+++ js/controllers.js
@@ -3,20 +3,27 @@ angular.module('djBooth.controllers', [])
 .controller('searchController', function ($scope, $window, searchSpotify){
 
   // the results array that houses the songs currently in the queue
-  $scope.results = [{songname: 'semi-charmed life', votes:  3}];
-  // this function calls the getData function in the SearchSpotify factory and returns the selected song
-  // this song is then added to the queue
-  $scope.getSong = function(){
-    SearchSpotify.getData($scope.song, function(data){
-      data.votes = 0;
-      $scope.results.push(data);
+  $scope.results = [{songname: 'Semi-Charmed Life', artist:'Third Eye Blind', votes:  3}, 
+  {songname: 'Tubthumping', artist:'Chumbawamba', votes: 0 }];
+  //this function adds whichever song is clicked in the search field to the playlist 
+  $scope.getSong = function(data){
+    $scope.results.push(data)
+    // the data will also need to be added to the db
+    // we should probably not even have a $scope.results but actually just post this to the db via the server
+    // on success to posting to the db we should do a get request and update the users view
     })
   }
 
 })
+// this is where the search field updates with data from spotify as you type, obviously this is not implemented and 
+// is placeholder code
 .directive('updateSearch', function(){
   return {
     link : function(scope, elem, attrs){
+          SearchSpotify.getData($scope.song, function(data){
+          // get the data from the server and add elements to the search dropdown so the user can select them
+          // 
+    })
       console.log('hi');
     }
   }
