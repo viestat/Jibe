089432d4671ae3cb0b8a25980fbb3ccf3392c4d8
Chris Saden <csaden@gmail.com>
2015-08-11 16:50:33 -0700

    added createPlaylist, findPlaylist, and hasSong functions

--- server/playlists/playlistController.js
+++ server/playlists/playlistController.js
@@ -4,6 +4,39 @@ var Playlist       = require('./playlistModel'),
 
 module.exports = {
 
+  createPlaylist: function(req, res, next) {
+    var playlist = new Playlist({'_creator': req.body.userId});
+    playlist.save(function(err, playlist) {
+      if (err) {
+        next(err);
+      } else {
+        res.json({'playlistId': playlist._id});
+      }
+    });
+  },
+
+  findPlaylist: function(req, res, next, playlistId) {
+
+    var findOne = Q.nbind(Playlist.findOne, Playlist);
+    var query = {"_id": playlistId};
+
+    findOne(query)
+      .then(function(playlist) {
+        if (playlist === null) {
+          next(new Error('Playlist Error: playlist not found in database.'));
+        } else {
+          req.playlist = playlist;
+        }
+      })
+      .fail(function (error) {
+        next(error);
+      });
+  },
+
+  hasSong: function(req, res, next, spotifyId) {
+    req.hasSong = req.playlist.hasSong(spotifyId);
+  },
+
   addSong: function(req, res, next) {
 
   },
