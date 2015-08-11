2d73e13869203e7442ffc31a7f15f2b044866803
Chris Saden <csaden@gmail.com>
2015-08-11 16:51:29 -0700

    routing and params for playlistId and songId

--- server/playlists/playlistRoutes.js
+++ server/playlists/playlistRoutes.js
@@ -3,6 +3,11 @@ var playlistController = require('./playlistController');
 module.exports = function (app) {
   // app === playlistRouter injected from middlware.js
 
-  app.post('/add', playlistController.addSong);
-  app.post('/remove', playlistController.removeSong);
+  app.param('playlistId', playlistController.findPlaylist);
+  app.param('spotifyId', playlistController.hasSong);
+
+
+  app.post('/create', playlistController.createPlaylist);
+  app.post('/add/:playlistId/:songId', playlistController.addSong);
+  app.post('/remove/:playlistId/:songId', playlistController.removeSong);
 };
