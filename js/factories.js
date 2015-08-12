3ed075d54653914e945baa82004fc53d811cf7a3
Robby Hays <rhaysvm42@gmail.com>
2015-08-12 13:26:13 -0700

    adds checkForPlaylist function in spotifyPlaylistMgr to determine if playlist needs to be initalized for the event; adds getIdentifiers helper function to databaseInteraction to retrieve user/playlist information needed for playlist GET/POST/PUT requests

--- js/factories.js
+++ js/factories.js
@@ -61,24 +61,20 @@ angular.module('djBooth.factories', [])
 })
 .factory('databaseInteraction', function($http){
 
-// this is our get request for our db for the current playlist in the room
-// this will be called when a user loads the room and whenever a succesful post request occurs to the db (so 
-// the user can see the updated playlist when after they add somethng to it)
-var getQueue = function($http){
-  // return $http({
-  //   method: 'GET',
-  //   url: 'OUR DB'
-  // })
-  // .then(function(resp){
-  //   return resp.data
-  // })
-  // SAMPLE DATA FOR TESTING
-  return [
-  {'song_name': 'Skin Deep', 'artists': ["Dusky"], 'spotifyId': '75kWsM3OTYyQYiEoWYAzH1'},
-  {'song_name': 'Simple Love', 'artists': ["Julio Bashmore"], 'spotifyId': '4grMB5gbgfMh6kUGmo45yf'}];
-};
+  // this is our get request for our db for the current playlist in the room
+  // this will be called when a user loads the room and whenever a succesful post request occurs to the db (so 
+  // the user can see the updated playlist when after they add somethng to it)
+  var getQueue = function($http){
+    return $http({
+      method: 'GET',
+      url: 'OUR DB'
+    })
+    .then(function(resp){
+      return resp.data
+    })
+  };
 
-// this is the post request for adding songs to our db and essentially the queue this is placeholder code
+  // this is the post request for adding songs to our db and essentially the queue this is placeholder code
   var addSong = function(songData){
       return $http({
       method: 'POST',
@@ -87,45 +83,81 @@ var getQueue = function($http){
     })
   };
 
-  // GET request for retrieving host's Spotify user_id from our database
-  // available once they authenticate
-  var getUserId = function($http){
+  // GET request for retrieving host's Spotify user_id, oauth token,
+  // and event's playlist Id from our database
+  var getIdentifiers = function($http){
     return $http({
       method: 'GET',
       url: 'OUR DB'
     })
     .then(function(resp){
-      return resp.data.userSpotifyId;
+      return {
+        userSpotifyId: resp.data.userSpotifyId,
+        eventId: resp.data._id,
+        eventName: resp.data.eventName,
+        userToken: resp.data.userToken
+      };
     })
   };
 
   return {
     getQueue: getQueue,
     addSong: addSong,
-    getUserId: getUserId
+    getIdentifiers: getIdentifiers
   }
 
 })
 // This factory will handle playlist creation, adding songs, and reordering them
 .factory('spotifyPlaylistMgr', function($http, databaseInteraction){
   
-  // Grab user's spotify id for post request
-  var userSpotifyId = databaseInteraction.getUserId();
-  var uri = 'https://api.spotify.com/v1/users/'+'robbyhays'/*userSpotifyId*/+'/playlists';
+  // Grab identifying info for user and event (playlist)
+  var userSpotifyId = databaseInteraction.getIdentifiers().userSpotifyId;
+  var eventId = databaseInteraction.getIdentifiers().eventId;
+  var eventName = databaseInteraction.getIdentifiers().eventName;
+  var userToken = databaseInteraction.getIdentifiers().userToken;
+
+  // uri for creating playlist
+  var uri = 'https://api.spotify.com/v1/users/'+userSpotifyId+'/playlists/';
+  // uri for accessing/modifying playlist
+  var playlistUri = uri + eventId;
   
-  // auth token from Robby's spotify for testing purposes --> will need to be replaced with
-  // user's auth token from authentication
+  // auth token (all permissions) from Robby's spotify for testing purposes
   var myToken = 'BQC6DciJdcfuCjygLrrIc1gttp6fwEK14QGa6AHf1oAbQCkoNi5-FFoQ1Hz8PAQkKVDIK3J0rCjs4Wh7m8rz6nvuMrBJOUrXy40Ul6oDglMuRA33OTN4_H_yvJdacCaYLDSZsre8Roa7ylMWJdrIrRStCxQDsfx0m0qS5EX3saZnD38qPyEj9-9nE13uxOXP8fI_noDWy0ZkiOfkM2PiuW8GIDNmuPJykA2hp3akXvEtGMa8c7PUSj6ff734nhR5s3wrmwXuMyhqER2X9VB60916lSf3kIQF3JreyrM_sUZdTg';
+  
+  // This function checks to see if playlist for room ID exists yet,
+  // and initializes new playlist if it does not
+  var checkForPlaylist = function($http){
+    // send a GET request to spotify with user ID and event playlist ID
+      // if response is 404, playlist does not exist yet and needs to be created
+    return $http({
+      method: 'GET',
+      url: playlistUri,
+      Accept: 'application/json',
+      Authorization: 'Bearer ' + userToken;
+    })
+    .then(function(resp){
+      if (resp.data.hasOwnProperty("error") && resp.data["error"]["status"] === 404){
+        initializePlaylist();
+      }
+      return resp.data;
+    })
+  };
+
+  // This function initializes a new playlist
   var initializePlaylist = function($http){
     return $http({
       method: 'POST',
       url: uri,
       Accept: 'application/json',
-      Authorization: 'Bearer ' + myToken,
+      Authorization: 'Bearer ' + userToken,
       data: {
         'name': 'JibeTestPlaylist1',
         'public': 'true'
       }
+    })
+    .then(function(resp){
+      // return playlist_id of the newly created playlist
+      return resp.data.id;
     })
   };
 
