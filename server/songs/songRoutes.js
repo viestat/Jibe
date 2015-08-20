d05adab1922fe849d5f26b5c79e905d5c2120c4d
mKurrels <mjkurrels@gmail.com>
2015-08-20 09:21:55 -0700

    intigrate loopback

--- server/songs/songRoutes.js
+++
@@ -1,8 +0,0 @@
-var songController = require('./songController');
-
-module.exports = function (app) {
-  // app === songRouter injected from middlware.js
-
-  app.post('/upvote', songController.upvote);
-  app.post('/downvote', songController.downvote);
-};
