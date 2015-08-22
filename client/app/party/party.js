ce3981f78f58ff5f3569d354da2073584aa0c834
Andrés C. Viesca Ruiz <viestat@icloud.com>
2015-08-21 18:43:13 -0700

    Demo fix

--- client/app/party/party.js
+++ client/app/party/party.js
@@ -7,7 +7,13 @@ party.controller('PartyController', function ($scope, $window, $location, getPla
     nowPlaying: {artist: 'Ozma', title: 'Domino Effect'}
   };
   //this asigns the respnse from the server to the queue variable
-  $scope.queue = getPlayList.playList($scope.party.name);
+  $scope.queue = [
+    {artist: 'Weezer', title: 'Buddy Holly', score: 5},
+    {artist: 'Nirvana', title: 'Smells Like Teen Spirit', score: 2},
+    {artist: 'Justin Bieber', title: 'Baby', score: -3},
+    {artist: 'Novos Baianos', title: 'Brasil Pandeiro', score: 15},
+  ];
+  // getPlayList.playList($scope.party.name);
 
 
   $scope.getNext = function(){
@@ -16,28 +22,22 @@ party.controller('PartyController', function ($scope, $window, $location, getPla
     $scope.queue.forEach(function(song){
       if(song.score >= max && !song.played){
         res = song.uri
-        song.played = true;
+        song.played = true; 
       } 
     });
   $scope.url = '//www.youtube.com/embed/'+ res + '?rel=0&autoplay=1';
   }
-  $scope.url = 'https://www.youtube.com/watch?v=oRSijEW_cDM'
-  // [
-  //   {artist: 'Weezer', title: 'Buddy Holly', score: 5},
-  //   {artist: 'Nirvana', title: 'Smells Like Teen Spirit', score: 2},
-  //   {artist: 'Justin Bieber', title: 'Baby', score: -3},
-  //   {artist: 'Novos Baianos', title: 'Brasil Pandeiro', score: 15},
-  // ];
+  $scope.url = '//www.youtube.com/embed/VAPJ_iieems?rel=0&autoplay=1'
 
 });
 party.factory('getPlayList', function($http){
-  var playList = function(party){
+  var playList = function(party){ 
     return $http({
       method: 'POST',
       url: '/api/getPlaylist',
-      data: {name: party}
-    })
-    .then(function(res){
+      data: {name: party} 
+    }) 
+    .then(function(res){ 
       return res.body;
     });
   };
